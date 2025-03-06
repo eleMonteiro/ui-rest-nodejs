6eff799c686c977470116b08c925bb24ef23dffa
Elenilson Monteiro <monteir.elenilson@gmail.com>
2025-03-06 12:35:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import api from './services/api';
+
+const data = ref(null);
+
+onMounted(async () => {
+  try {
+    const response = await api.get('/endpoint');
+    data.value = response.data;
+  } catch (error) {
+    console.error('Erro ao buscar dados', error);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Dados do Servidor</h1>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
